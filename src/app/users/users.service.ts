d677869e0975f031c9550c6141fdce934d7c362c
Ruslan <afro.funky.lover@gmail.com>
2019-10-16 05:49:00 +0700

    remove delay

--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { delay, map } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { User } from './user.interface';
 
 @Injectable({
@@ -14,8 +14,7 @@ export class UsersService {
     return this.http.get<{ results: User[] }>('/raw/task/users.json').pipe(
       map(({ results }) => {
         return results.map(r => ({ ...r, dob: new Date(r.dob) }));
-      }),
-      delay(2000)
+      })
     );
   }
 }
